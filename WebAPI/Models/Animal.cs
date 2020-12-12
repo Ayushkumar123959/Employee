0b5b03f92e9bd0069bfb7542c5f4403ce5b63c6e
AyushKumar123959 <a.k1297777@gmail.com>
2020-12-12 17:32:53 +0530

    Ayush

---
+++ WebAPI/Models/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string AnimalName { get; set; }
+
+        public string AnimalType  { get; set; }
+
+        public string Category { get; set; }
+
+
+    }
+}
